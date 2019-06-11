94048a0155e0431072b54f0e73a8b44c04b7a970
Ry DeSoto <rdesoto@itgny.com>
2019-06-11 18:36:42 -0500

    Unit testing part 1

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-/* 
-Commented out default test due to but in React <16.9. The big 
+/*
+Commented out default test due to but in React <16.9. The big
 red warning is very distracting for the workshop.
 Bug documented here:
 https://github.com/testing-library/react-testing-library/issues/281
@@ -14,3 +14,26 @@ https://github.com/testing-library/react-testing-library/issues/281
 //   ReactDOM.render(<App />, div);
 //   ReactDOM.unmountComponentAtNode(div);
 // });
+
+function sum(a,b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error();
+  } else {
+    return a+b;
+  }
+}
+
+describe ("sum", () => {
+  it("should add 2 positive numbers", () => {
+    expect(sum(2,3)).toEqual(5);
+  })
+
+  it("should add 2 negative numbers", () => {
+    expect(sum(-2,-3)).toEqual(-5);
+  })
+
+  // for error handling -- need to pass a function to expect
+  it("should throw an error if a param is bad", () => {
+    expect(() => sum("2", 3)).toThrowError();
+  })
+})
