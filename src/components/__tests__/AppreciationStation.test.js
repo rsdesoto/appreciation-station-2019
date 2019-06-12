c4da2446cca368cbde486238189101a44134cb5b
Ry DeSoto <rdesoto@itgny.com>
2019-06-11 19:14:04 -0500

    React testing suite

--- src/components/__tests__/AppreciationStation.test.js
+++ src/components/__tests__/AppreciationStation.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, expectExport } from "@testing-library/react";
 
 import AppreciationStation from "../AppreciationStation";
 
@@ -63,4 +63,41 @@ const mockThankYous = [
 ];
 
 // automatically unmount and cleanup DOM after the test is finished.
+// React testing library actually renders the DOM -- need to clean up every time
 afterEach(cleanup);
+
+describe("AppreciationStation", () => {
+  describe("render", () => {
+    it("renders a thank you card by default", () => {
+      // const queries = render(<AppreciationStation thankYous={mockThankYous} />)
+      // react testing library will return an object with queries so you can look into the DOM to find things
+      // will often see `const { getByLabelText } = render(<AppreciationStation thankYous={mockThankYous} />)`
+      const { getByLabelText } = render(
+        <AppreciationStation thankYous={mockThankYous} />
+      );
+
+      const thankYouCanvas = getByLabelText(/thank you test member/i)
+      expect(thankYouCanvas).not.toBeNull();
+    })
+  })
+
+  describe("clicking next", () => {
+    it("displays the next thank you card", () => {
+      // test what the user will see
+      const { getByLabelText, getByAltText } = render(
+        <AppreciationStation thankYous={mockThankYous} />
+      );
+
+      const nextButton = getByAltText(/go to next/i)
+      fireEvent.click(nextButton);
+
+      const thankYouCanvas = getByLabelText(/thank you test member 2/i);
+      expect(thankYouCanvas).not.toBeNull();
+    })
+  })
+})
+
+
+
+
+
